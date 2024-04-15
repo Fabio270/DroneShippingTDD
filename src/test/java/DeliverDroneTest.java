96da7d5347a392d7b9aea4c2c82901aa3d600e33
Fábio Seyiji <81870980+Fabio270@users.noreply.github.com>
2024-04-15 20:59:58 -0300

    Refactor (DeliverDroneTest): extracted constructor to beforeEach

--- src/test/java/DeliverDroneTest.java
+++ src/test/java/DeliverDroneTest.java
@@ -1,4 +1,5 @@
 import DeliverDrone.Drone;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,12 +9,19 @@ import java.io.PrintStream;
 import static org.assertj.core.api.Assertions.*;
 
 public class DeliverDroneTest {
+    private Drone drone;
+    private ByteArrayOutputStream outputStream;
+    @BeforeEach
+    public void setUp(){
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        drone = new Drone();
+    }
+
     @Test
     @DisplayName("Should the drone get non fitting box measurements and return N")
     void shouldTheDroneGetNonFittingBoxAndWindowMeasurementsAndReturnN(){
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
-        Drone drone = new Drone();
+
         drone.getMeasurements(75, 100, 50, 100, 30);
         drone.isBoxFitting();
         String printedOutput = outputStream.toString().trim();
@@ -23,9 +31,6 @@ public class DeliverDroneTest {
     @Test
     @DisplayName("Should the drone get fitting box measurements and return S")
     void shouldTheDroneGetFittingBoxAndWindowMeasurementsAndReturnS(){
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
-        Drone drone = new Drone();
         drone.getMeasurements(30, 50, 80, 80, 60);
         drone.isBoxFitting();
         String printedOutput = outputStream.toString().trim();
