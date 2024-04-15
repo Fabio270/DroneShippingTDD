e9d81b47783057676d35faec166a2f8e4dba026a
Fábio Seyiji <81870980+Fabio270@users.noreply.github.com>
2024-04-15 19:55:23 -0300

    Feat(Drone): Created Drone class and its methods assinature.

---
+++ src/main/java/DeliverDrone/Drone.java
@@ -0,0 +1,11 @@
+package DeliverDrone;
+
+public class Drone {
+    public void getMeasurements(int a, int b, int c, int h, int l) {
+    }
+
+
+    public boolean isBoxFiting() {
+        return false;
+    }
+}
--- src/test/java/DeliverDroneTest.java
+++ src/test/java/DeliverDroneTest.java
@@ -1,3 +1,4 @@
+import DeliverDrone.Drone;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,7 +8,7 @@ public class DeliverDroneTest {
     @DisplayName("Should the drone get non fitting box measurements and return N")
     void shouldTheDroneGetBoxAndWindowMeasurements(){
         Drone drone = new Drone();
-        drone.getMeasurements();
+        drone.getMeasurements(75, 100, 50, 100, 30);
         drone.isBoxFiting();
         Assertions.assertThat(drone.isBoxFiting()).isFalse();
     }
