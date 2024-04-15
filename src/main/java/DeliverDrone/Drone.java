52051c4b1e80a0d313bd4c2c2d32a66e63d770c0
Fábio Seyiji <81870980+Fabio270@users.noreply.github.com>
2024-04-15 20:05:55 -0300

    Feat (Drone): Implemented getMeasurements method.

--- src/main/java/DeliverDrone/Drone.java
+++ src/main/java/DeliverDrone/Drone.java
@@ -1,11 +1,60 @@
 package DeliverDrone;
 
 public class Drone {
+    private int boxA;
+    private int boxB;
+    private int boxC;
+    private int windowh;
+    private int windowl;
     public void getMeasurements(int a, int b, int c, int h, int l) {
+        setBoxA(a);
+        setBoxB(b);
+        setBoxC(c);
+        setWindowh(h);
+        setWindowl(l);
     }
 
+    public int getBoxA() {
+        return boxA;
+    }
 
-    public boolean isBoxFiting() {
+    public boolean isBoxFitting() {
         return true;
+    }
+
+    public void setBoxA(int boxA) {
+        this.boxA = boxA;
+    }
+
+    public int getBoxB() {
+        return boxB;
+    }
+
+    public void setBoxB(int boxB) {
+        this.boxB = boxB;
+    }
+
+    public int getBoxC() {
+        return boxC;
+    }
+
+    public void setBoxC(int boxC) {
+        this.boxC = boxC;
+    }
+
+    public int getWindowh() {
+        return windowh;
+    }
+
+    public void setWindowh(int windowh) {
+        this.windowh = windowh;
+    }
+
+    public int getWindowl() {
+        return windowl;
+    }
+
+    public void setWindowl(int windowl) {
+        this.windowl = windowl;
     }
 }
