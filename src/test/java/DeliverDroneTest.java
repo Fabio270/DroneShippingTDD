992209c4f0bf3d126a7418e1f9ddda51fcbc94e5
Fábio Seyiji <81870980+Fabio270@users.noreply.github.com>
2024-04-15 20:41:15 -0300

    Test: changed tests to expect string S or N and not boolean.

--- src/test/java/DeliverDroneTest.java
+++ src/test/java/DeliverDroneTest.java
@@ -2,24 +2,33 @@ import DeliverDrone.Drone;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class DeliverDroneTest {
     @Test
     @DisplayName("Should the drone get non fitting box measurements and return N")
     void shouldTheDroneGetNonFittingBoxAndWindowMeasurementsAndReturnN(){
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
         Drone drone = new Drone();
         drone.getMeasurements(75, 100, 50, 100, 30);
         drone.isBoxFitting();
-        assertThat(drone.isBoxFitting()).isFalse();
+        String printedOutput = outputStream.toString().trim();
+        assertThat(printedOutput).isEqualTo("N");
     }
 
     @Test
     @DisplayName("Should the drone get fitting box measurements and return S")
     void shouldTheDroneGetFittingBoxAndWindowMeasurementsAndReturnS(){
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
         Drone drone = new Drone();
         drone.getMeasurements(30, 50, 80, 80, 60);
         drone.isBoxFitting();
-        assertThat(drone.isBoxFitting()).isTrue();
+        String printedOutput = outputStream.toString().trim();
+        assertThat(printedOutput).isEqualTo("S");
     }
 }
