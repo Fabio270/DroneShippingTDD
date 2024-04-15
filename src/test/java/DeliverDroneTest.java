1247721fa1b431b7bb2e52a7a57fda7154e1846f
Fábio Seyiji <81870980+Fabio270@users.noreply.github.com>
2024-04-15 20:15:30 -0300

    refactor DeliverDroneTest: changed Tests names to be more descriptive

--- src/test/java/DeliverDroneTest.java
+++ src/test/java/DeliverDroneTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class DeliverDroneTest {
     @Test
     @DisplayName("Should the drone get non fitting box measurements and return N")
-    void shouldTheDroneGetBoxAndWindowMeasurementsAndReturnN(){
+    void shouldTheDroneGetNonFittingBoxAndWindowMeasurementsAndReturnN(){
         Drone drone = new Drone();
         drone.getMeasurements(75, 100, 50, 100, 30);
         drone.isBoxFitting();
@@ -15,7 +15,7 @@ public class DeliverDroneTest {
 
     @Test
     @DisplayName("Should the drone get fitting box measurements and return S")
-    void shouldTheDroneGetBoxAndWindowMeasurementsAndReturnS(){
+    void shouldTheDroneGetFittingBoxAndWindowMeasurementsAndReturnS(){
         Drone drone = new Drone();
         drone.getMeasurements(30, 50, 80, 80, 60);
         drone.isBoxFitting();
