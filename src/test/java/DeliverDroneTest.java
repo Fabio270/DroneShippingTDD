bbbe38da1a0bbe43abc5244fdfed82ed13903989
Fábio Seyiji <81870980+Fabio270@users.noreply.github.com>
2024-04-15 21:14:46 -0300

    Test: changed tests to parameterized tests.

--- src/test/java/DeliverDroneTest.java
+++ src/test/java/DeliverDroneTest.java
@@ -2,6 +2,8 @@ import DeliverDrone.Drone;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -11,27 +13,37 @@ import static org.assertj.core.api.Assertions.*;
 public class DeliverDroneTest {
     private Drone drone;
     private ByteArrayOutputStream outputStream;
+
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
         drone = new Drone();
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "75, 100, 50, 100, 30",
+            "101, 60, 50, 100, 30",
+            "50, 70, 90, 60, 65"
+    })
     @DisplayName("Should the drone get non fitting box measurements and return N")
-    void shouldTheDroneGetNonFittingBoxAndWindowMeasurementsAndReturnN(){
-
-        drone.getMeasurements(75, 100, 50, 100, 30);
+    void shouldTheDroneGetNonFittingBoxAndWindowMeasurementsAndReturnN(int a, int b, int c, int h, int l) {
+        drone.getMeasurements(a, b, c, h, l);
         drone.isBoxFitting();
         String printedOutput = outputStream.toString().trim();
         assertThat(printedOutput).isEqualTo("N");
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "50, 30, 100, 80, 60",
+            "30, 50, 80, 80, 60",
+            "80, 60, 100, 80, 60"
+    })
     @DisplayName("Should the drone get fitting box measurements and return S")
-    void shouldTheDroneGetFittingBoxAndWindowMeasurementsAndReturnS(){
-        drone.getMeasurements(30, 50, 80, 80, 60);
+    void shouldTheDroneGetFittingBoxAndWindowMeasurementsAndReturnS(int a, int b, int c, int h, int l) {
+        drone.getMeasurements(a, b, c, h, l);
         drone.isBoxFitting();
         String printedOutput = outputStream.toString().trim();
         assertThat(printedOutput).isEqualTo("S");
